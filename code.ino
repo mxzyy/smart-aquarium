8ec4bcf5afda532a08cd41c5f68a9992408239b2
mxzyy <94878524+mxzyy@users.noreply.github.com>
2023-03-06 22:20:21 +0700

    Update code.ino

--- code.ino
+++ code.ino
@@ -4,7 +4,10 @@
 #include <BlynkSimpleEsp8266.h>
 
 #define ServoPin 12 // D6
-#define DS18Pin 2 //D4
+#define DS18Pin 2 // D4
+#define RelayPin 14 // D5
+#define RelayIn 15 // D8
+#define ServoIn 16 // D0
 
 BlynkTimer timer;
 Servo MG90s;
@@ -17,12 +20,15 @@ char pass[] = "pecellele";
 
 void sendTemp(){
   sensors.requestTemperatures();
-  int t = sensors.getTempFByIndex(0);
+  int t = sensors.getTempCByIndex(0);
   Blynk.virtualWrite(V0, t);  
 }
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(RelayIn, INPUT);
+  pinMode(ServoIn, INPUT);
+  pinMode(RelayPin, OUTPUT);  
   Serial.begin(96000);
   MG90s.attach(ServoPin);
   MG90s.write(0);
@@ -43,10 +49,24 @@ BLYNK_WRITE(V1){
   }
 }
 
+BLYNK_WRITE(V2){
+  int pinValue = param.asInt();
 
+  if (pinValue == 1){
+    digitalWrite(RelayPin, HIGH);
+  }else{
+    digitalWrite(RelayPin, LOW);
+  }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
+  bool switchStat = digitalRead(RelayIn);
+  if (switchStat == LOW){
+    digitalWrite(RelayPin, HIGH);
+  }else{
+    digitalWrite(RelayPin, LOW);
+  }
   Blynk.run();
   timer.run();
 }
